7bf6e5e006f571524e0d395591f9c975ea76b5af
digbanmije18@gmail.com <digbanmije18@gmail.com>
2025-03-08 23:57:56 +0100

    changes

--- src/Components/Pages/Contact/Contact.tsx
+++ src/Components/Pages/Contact/Contact.tsx
@@ -3,11 +3,11 @@ import questions from "../../JSON/Questions.json";
 
 const Contact = () => {
   return (
-    <div className="w-[100%] h-[35rem] flex justify-center items-center bg-[gray]">
-      <div className="w-[95%] grid gap-[10px] grid-cols-1 lg:grid-cols-3 h-[95%] bg-[green]  ">
-        {questions.map((id, question, answer) => (
+    <div className="w-[100%] min-h-[35rem] flex justify-center items-center bg-[gray]">
+      <div className="w-[95%]  grid gap-[10px] grid-cols-1 lg:grid-cols-3 h-[95%] bg-[green]  ">
+        {questions.map((el: any) => (
           <div>
-            <Flashcards id={id} question={question} answer={answer} />
+            <Flashcards flashObj={el} key={el.id} />
           </div>
         ))}
       </div>
--- src/Components/ProductsCards/Flashcards.tsx
+++ src/Components/ProductsCards/Flashcards.tsx
@@ -1,7 +1,21 @@
-const Flashcards = ({ id, question, answer }: any) => {
+import { useState } from "react";
+
+const Flashcards = ({ flashObj }: any) => {
+  const [selectedId, setSelectedId] = useState(null);
+
+  const handleClick = (id: any) => {
+    setSelectedId(id);
+  };
   return (
-    <div className="min-h-[250px] flex justify-center items-center rounded-md bg-[white]">
-      {question}
+    <div
+      className={
+        flashObj.id === selectedId
+          ? "min-h-[300px] flex justify-center items-center rounded-md bg-[red]"
+          : "min-h-[300px] flex justify-center items-center rounded-md bg-[white]"
+      }
+      onClick={() => handleClick(flashObj.id)}
+    >
+      <p>{flashObj.id === selectedId ? flashObj.answer : flashObj.question}</p>
     </div>
   );
 };
